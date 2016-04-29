b4542ae45f79cd02a080353ec5d246c6456222a0
Ijem <ij.ofili@gmail.com>
2016-04-29 21:53:54 +0100

    added entry
    refresher functions

---
+++ rtJson/wranglingloops.js
@@ -0,0 +1,34 @@
+//global variables
+var RESULT, SCORE, MENU, QUESTION, QUESTIONS, OPTION, OPTIONS;
+
+//the Score
+/*=====================*/
+
+if(option===true){
+    result = result + 1;
+};
+
+//while(){};
+
+//the Menu
+/*=====================*/
+
+//the Question
+/*=====================*/
+var option1, option2, option3;
+OPTION = [];
+OPTION
+
+if(option===true){
+    
+};
+
+//while(){};
+/*=====================*/
+
+//the Timer
+/*=====================*/
+
+//the options
+/*=====================*/
+
---
+++ sample_.js
@@ -0,0 +1,7 @@
+//var r, t, d, k, a, b;
+
+var function = divide(a, b){
+    return a + b;
+}
+
+divide(3, 7);
